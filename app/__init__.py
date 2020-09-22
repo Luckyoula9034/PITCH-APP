4381576e9fc8156b469b1e0b7484cc16ce46a43b
luqqy <you@example.com>
2020-09-22 16:10:23 +0300

    added a navbar

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from .config import DevConfig
 from flask_bootstrap import Bootstrap
 # Initializing application
-app = Flask(__name__,instance_relative_config = True)
+app = Flask(__name__)
 
 # Setting up configuration
 app.config.from_object(DevConfig)
